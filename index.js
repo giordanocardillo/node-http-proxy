2c2433252521b4f5d581faca3bf7f20414e4f659
Giordano Cardillo <giordano.cardillo@gmail.com>
2019-02-06 03:38:50 -0500

    Fixed typo in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const ProxyChain = require('proxy-chain')
 const port = 80;
-const server = new ProxyChain.server({port})
+const server = new ProxyChain.Server({port})
 
 server.listen(() => {
   console.info(`ÂProxy listening on port $port}`)
